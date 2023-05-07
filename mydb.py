275b658559f5de88dd2ad10b31beb509f51ca190
Niki062004 <nikitaphaple06@gmail.com>
2023-05-07 21:55:04 +0530

    Initial Commit

---
+++ mydb.py
@@ -0,0 +1,13 @@
+import mysql.connector
+dataBase=mysql.connector.connect(
+    host='localhost',
+    user='root',
+    passwd='Nikita#06'
+)
+#prepare a cursor object
+cursorObject=dataBase.cursor()
+
+#Create a database
+cursorObject.execute("CREATE DATABASE elderco")
+
+print("All Done!")
